3babc0b3ad1deaf5f69a57b289445f2fd7118de4
A7medAbdien <ahmed.g.abdien@gmail.com>
2025-01-09 17:34:58 +0300

    was with the previous commit, remove theprimeagen treesitter and fix opening dap in omerxx

--- lua/omerxx/lazy/plugins/dap.lua
+++ lua/omerxx/lazy/plugins/dap.lua
@@ -4,7 +4,9 @@ require('nvim-dap-virtual-text').setup()
 vim.fn.sign_define('DapBreakpoint', { text='🔴', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
 
 -- Debugger
-vim.api.nvim_set_keymap("n", "<leader>dt", ":DapUiToggle<CR>", {noremap=true})
+vim.keymap.set('n', '<leader>dt', function()
+  require('dapui').toggle()
+end, { desc = 'Toggle DAP UI' })
 vim.api.nvim_set_keymap("n", "<leader>db", ":DapToggleBreakpoint<CR>", {noremap=true})
 vim.api.nvim_set_keymap("n", "<leader>dc", ":DapContinue<CR>", {noremap=true})
 vim.api.nvim_set_keymap("n", "<leader>dr", ":lua require('dapui').open({reset = true})<CR>", {noremap=true})
